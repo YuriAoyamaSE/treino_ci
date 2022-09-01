4c60a936fcc32e06571cd0e1037e1efea5685acf
YuriAoyamaSE <aoyamabrasil@gmail.com>
2022-09-01 15:12:56 -0300

    first commit

---
+++ locadora/serializers/cliente_serializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from locadora.models.cliente import Cliente
+
+
+class ClienteSerializer(serializers.ModelSerializer):
+    nome = serializers.CharField(min_length=10,  max_length=120)
+
+    class Meta:
+        model = Cliente
+        fields = "__all__"
